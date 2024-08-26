7440ebcfa18f0e9f448f1594b070d507459783cf
wilmer <wilmer7522@gmail.com>
2024-08-26 06:37:41 -0500

    feat :sparkles: working code

--- Dia5/script.js
+++ Dia5/script.js
@@ -1,127 +1,157 @@
 fetch("datos.json")
     .then(Informacion => Informacion.json())
     .then(Datos => {
-        console.log(Datos);
+
+        const p0 = document.getElementById("p0");
+        const bloque3 =  document.getElementById("tres");
+
         
+
+        boton1.addEventListener("click", (e) => {
+            
+            searchProducts()
+        })
+
 
         function date() {
             const mesActual = new Date();
             const meses = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
             console.log(`${mesActual.getDate()}-${meses[mesActual.getMonth()]}-${mesActual.getFullYear()}`);
-            
-            
-            
+
+
+
         }
-        date()
-        
-                                                                    //PRODUCTOS
-        
-        
+        //date()
+
+        console.log(`|---- SEVEN ELEVEN ----|`);
+
+
+
+
+        //PRODUCTOS
+
+
         //Agregar Productos
-        function addProduct(product){
-            
+        function addProduct(product) {
+
         }
-        
-        
+
+
         //Leer Productos
-        function viewProducts(){
-        
+        function viewProducts() {
+
         }
-        
+
         //Actualizar Productos
-        function updateProduct(id, newDetails){
-        
+        function updateProduct(id, newDetails) {
+
         }
-        
+
         //Eliminar Productos
-        function deleteProduct(id){
-        
+        function deleteProduct(id) {
+
         }
-        
-                                                                //PROVEEDORES
-        
+
+        //PROVEEDORES
+
         //Agregar Proveedores
-        function addSupplier(supplier){
-        
+        function addSupplier(supplier) {
+
         }
-        
+
         //Leer proveedores
-        function viewSuppliers(){
-        
+        function viewSuppliers() {
+
         }
-        
+
         //Actualizar Proveedores
-        function updateSupplier(id, newDetails){
-        
+        function updateSupplier(id, newDetails) {
+
         }
-        
+
         //Eliminar proveedores
-        function deleteSupplier(id){
-        
+        function deleteSupplier(id) {
+
         }
-        
-                                                            //ORDENES
-        
+
+        //ORDENES
+
         //Agregar Ordenes
-        function addOrder(order){
-        
+        function addOrder(order) {
+
         }
-        
+
         //Leer Ordenes
-        function viewOrders(){
-        
+        function viewOrders() {
+
         }
-        
+
         //Actualizar Ordenes
-        function updateOrder(orderId, newDetails){
-        
+        function updateOrder(orderId, newDetails) {
+
         }
-        
+
         //Eliminar Ordenes
-        function deleteOrder(orderId){
-        
+        function deleteOrder(orderId) {
+
         }
-        
-                                                        //STOCK
-        
+
+        //STOCK
+
         //Verificar Stock
-        function checkStockLevels(){
-        
+        function checkStockLevels() {
+
         }
-        
+
         //Agregar Stock
-        function restockProduct(id, quantity){
-        
+        function restockProduct(id, quantity) {
+
         }
-        
-                                                                //REPORTES
-        
+
+        //REPORTES
+
         //Reporte de Ventas Generales
-        function generateSalesReport(startDate, endDate){
-        
+        function generateSalesReport(startDate, endDate) {
+
         }
-        
+
         //Generar Reportes de Inventario
-        function generateInventoryReport(){
-        
+        function generateInventoryReport() {
+
         }
-        
-                                                                            //BUSCAR y FILTRAR
+
+        //BUSCAR y FILTRAR
         //Buscar Productos
-        function searchProducts(query){
-            for (i of Datos){
+        function searchProducts() {
+            cont = 1
+            p0.textContent = "|----Products----|"
+            
+            for (let i of Datos.products) {
+                bloque3.innerHTML += `<br>Id: ${i["id"]}  `
+                bloque3.innerHTML += `<br>Name: ${i["name"]}`
+                bloque3.innerHTML += `<br>Category: ${i["category"]}`
+                bloque3.innerHTML += `<br>Price: ${i["price"]}`
+                bloque3.innerHTML += `<br>Quantity: ${i["quantityInStock"]}`
+                bloque3.innerHTML += `<br>Supplier Id: ${i["supplierId"]}<br>`
+
                 
-                console.log(i);
-                
+            }
 
             }
-        }
-        searchProducts(query)
         
+        //searchProducts()
+
+
         //Filtrar Ordenes
-        function filterOrders(criteria){
-        
+        function filterOrders(criteria) {
+            for (let i in Datos.orders) {
+                console.log(`Status: ${Datos.orders[i].status}`);
+
+            }
+
         }
+        //filterOrders()
+
 
     })
 
