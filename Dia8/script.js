675b24c347851ab336a6ba5602bdb12abfa34392
wilmer <wilmer7522@gmail.com>
2024-09-01 23:07:25 -0500

    feat :sparkles: working code: created world, species and vehicles

--- Dia8/script.js
+++ Dia8/script.js
@@ -3,18 +3,27 @@ document.getElementById("boton").addEventListener("click", () => {
 
     url = `https://swapi.py4e.com/api/people/${input}`
 
-    
-    const body = document.getElementById("tablaPeople")
 
+    const body = document.getElementById("tablaPeople")
     const column = document.createElement("tbody")
+    const column2 = document.getElementById("tbody")
+    const peli= document.getElementById("peli")
+
 
 
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data.mass);
-            
-            column.innerHTML = `
+
+            const homeWorld = data.homeworld
+
+            fetch(homeWorld)
+                .then(res => res.json())
+                .then(home => {
+
+                    
+
+                    column.innerHTML = `
             <tr><th>Name</th><td>${data.name}</td><tr>
             <tr><th>Height</th><td>${data.height}</td></tr>
             <tr><th>Mass</th><td>${data.mass}</td></tr>
@@ -24,12 +33,131 @@ document.getElementById("boton").addEventListener("click", () => {
             <tr><th>Birth Year</th><td>${data.birth_year}</td></tr>
             <tr><th>Gender</th><td>${data.gender}</td></tr>
             <tr><th>HomeWorld</th><td>
+            <ul class="list-group">
+            <li class="list-group-item">Name: ${home.name}</li>
+            <li class="list-group-item">Rotation Period: ${home.rotation_period}</li>
+            <li class="list-group-item">Orbital Period: ${home.orbital_period}</li>
+            <li class="list-group-item">Diameter: ${home.diameter}</li>
+            <li class="list-group-item">Climate: ${home.climate}</li>
+            <li class="list-group-item">Gravity: ${home.gravity}</li>
+            <li class="list-group-item">Terrain: ${home.terrain}</li>
+            <li class="list-group-item">Surface Water: ${home.surface_water}</li>
+            <li class="list-group-item">Population: ${home.population}</li>
+            <li class="list-group-item">Residents: ${home.residents}</li>
+            <li class="list-group-item">Films: ${home.films}</li>
+            <li class="list-group-item">Created: ${home.created}</li>
+            <li class="list-group-item">Edited: ${home.edited}</li>
+            <li class="list-group-item">Url: ${home.url}</li>
+            </ul></td><tr>
 
-
-            <ul class="list-group"><li class="list-group-item">${data.films}</li></ul></td><tr>
-            `
-            body.appendChild(column)
+            <tr>Films</tr>
             
-        })
+            
 
+
+            
+            
+
+            `
+            
+
+                        
+                    const peliculas = data.films
+                    peliculas.forEach(pelis => {
+                        fetch(pelis)
+
+                            .then(res => res.json())
+                            .then(film => {
+
+
+
+                                
+                                column.innerHTML += `                                
+                                <tr><th><td>
+                                <ul class="list-group">
+                                <li class="list-group-item">Title: ${film.title}</li>
+                                <li class="list-group-item">Episodie Id: ${film.episode_id}</li>
+                                <li class="list-group-item">Opening Crawl: ${film.opening_crawl}</li>
+                                <li class="list-group-item">Director: ${film.director}</li>
+                                <li class="list-group-item">Producer: ${film.producer}</li>
+                                <li class="list-group-item">Release Date: ${film.release_date}</li>
+                                <li class="list-group-item">Characters: ${film.characters}</li>
+                                </ul></td></th><tr>
+                                `
+                })
+            })
+                            
+
+                            const especies = data.species
+                        fetch(especies)
+                        .then(res => res.json())
+                        .then(especie => {
+                            console.log(especie.name, especie.classification)
+
+                            column.innerHTML += `                                
+                            <tr><th>Species<td>
+                            <ul class="list-group">
+                            <li class="list-group-item">Name: ${especie.name}</li>
+                            <li class="list-group-item">Clasification: ${especie.classification}</li>
+                            <li class="list-group-item">Designation: ${especie.designation}</li>
+                            <li class="list-group-item">Average Heght: ${especie.average_height}</li>
+                            <li class="list-group-item">Skin  Colors: ${especie.skin_colors}</li>
+                            <li class="list-group-item"> Hair Colors: ${especie.hair_colors}</li>
+                            <li class="list-group-item"> Eye Colors: ${especie.eye_colors}</li>
+                            <li class="list-group-item"> Average Lifespan: ${especie.average_lifespan}</li>
+                            <li class="list-group-item">  Homeworld: ${especie.homeworld}</li>
+                            <li class="list-group-item">  Language: ${especie.language}</li>
+                            <li class="list-group-item"> People: ${especie.people}</li>
+                            <li class="list-group-item">Films: ${especie.films}</li>
+                            <li class="list-group-item">Created: ${especie.created}</li>
+                            <li class="list-group-item">Edited: ${especie.edited}</li>
+                            <li class="list-group-item">Url: ${especie.url}</li>
+                            </ul></td>Vehicles</th><tr>`
+                            
+
+                    })
+
+                    const vehiculos = data.vehicles
+                vehiculos.forEach(carros => {
+                    fetch(carros)
+                    .then(res => res.json())
+                    .then(vehiculo => {
+                        console.log(vehiculo.name, vehiculo.model, vehiculo.vehicle_class)
+
+                        column.innerHTML  += `
+                        <tr><th><td>
+                        <ul class="list-group">
+                        <li class="list-group-item">Name: ${vehiculo.name}</li>
+                        <li class="list-group-item">Model: ${vehiculo.model}</li>
+                        <li class="list-group-item">Vehicle Class: ${vehiculo.vehicle_class}</li>
+                        <li class="list-group-item">Manufacturer: ${vehiculo.manufacturer}</li>
+                        <li class="list-group-item">Cost in Credits: ${vehiculo.cost_in_credits}</li>
+                        <li class="list-group-item">Length: ${vehiculo.length}</li>
+                        <li class="list-group-item">Max Atmosphering Speed: ${vehiculo.max_atmosphering_speed}</li>
+                        <li class="list-group-item">Crew: ${vehiculo.crew}</li>
+                        <li class="list-group-item">Passengers: ${vehiculo.passengers}</li>
+                        <li class="list-group-item">Cargo Capacity: ${vehiculo.cargo_capacity}</li>
+                        <li class="list-group-item">Consumables: ${vehiculo.consumables}</li>
+                        <li class="list-group-item">Vehicles Class: ${vehiculo.vehicles_class}</li>
+                        <li class="list-group-item">Pilots: ${vehiculo.pilots}</li>
+                        <li class="list-group-item">Films: ${vehiculo.films}</li>
+                        <li class="list-group-item">Created: ${vehiculo.created}</li>
+                        <li class="list-group-item">Edited: ${vehiculo.edited}</li>
+                        <li class="list-group-item">Url: ${vehiculo.url}</li>
+                        </ul></td></th></tr>
+                        
+                        `
+
+
+                    })
+                })
+
+                    
+
+                })
+
+                
+
+        })
+        body.appendChild(column)
 })
