ce10e4cc688af354ea3a4d1cd85e11d0db1754cc
wilmer <wilmer7522@gmail.com>
2024-09-02 22:34:31 -0500

    feat :tada: code finished

--- Dia9/script.js
+++ Dia9/script.js
@@ -1,19 +1,53 @@
-document.getElementById("entrada").addEventListener('input', () =>{
-    
+document.getElementById("entrada").addEventListener('input', () => {
+
     let input = document.getElementById("entrada").value
 
-let muestra = document.getElementById("muestra")
-    fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
-        .then(res => res.json())
-        .then(pokemon => {
-            console.log(pokemon.name);
+    let muestra = document.getElementById("muestra")
 
-            muestra.innerHTML =`
-            ${pokemon.id}
-            ${pokemon.name}
-            <img src="${pokemon.sprite.other.showdown.front_default}>`
+    //let poke = `https://pokeapi.co/api/v2/pokemon/`
+
+    function pokeApi() {
 
 
-            
-        })
+        fetch(`https://pokeapi.co/api/v2/pokemon/`+ input)
+            .then(res => res.json())
+            .then(pokemon => {
+
+                
+                muestra.innerHTML = `
+            <p id="a">${pokemon.id} -</p>  <p>${pokemon.name}</p>`
+
+
+                let foto = document.getElementById("dibujoDiv");
+
+                foto.innerHTML = ` 
+
+            <audio autoplay src="${pokemon.cries.latest}">
+        </audio>
+            <img id="dibujo" src="${pokemon.sprites.other.showdown.front_default}">`
+
+
+
+                document.getElementById("previus").addEventListener("click", () => {
+                    prev = pokemon.id - 1
+                    input = String(prev)
+                    pokeApi()
+
+
+                })
+
+                document.getElementById("next").addEventListener("click", () => {
+                    prev = pokemon.id + 1
+                    input = String(prev)
+                    pokeApi()
+
+                })
+
+
+            })
+
+
+    }
+
+    pokeApi()
 })
