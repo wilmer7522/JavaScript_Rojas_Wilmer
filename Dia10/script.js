6c27821a18617e1bea7ef98bcb48c16f7eab0708
wilmer <wilmer7522@gmail.com>
2024-09-03 22:14:34 -0500

    feat :tada: code JS finished

--- Dia10/script.js
+++ Dia10/script.js
@@ -1,28 +1,38 @@
-//let url = `https://api.disneyapi.dev/character/`
+document.getElementById("buscar").addEventListener("input", () => {
+    buscarPersonaje();
+});
 
-document.getElementById("boton").addEventListener("click", () => {
-    let ver = document.getElementById("output")
+document.getElementById("siguiente").addEventListener("click", () => {
+    let buscar = document.getElementById("buscar");
+    buscar.value = parseInt(buscar.value) + 1;
+    buscarPersonaje();
+});
 
-    let buscar = document.getElementById("buscar").value
+document.getElementById("atras").addEventListener("click", () => {
+    let buscar = document.getElementById("buscar");
+    buscar.value = parseInt(buscar.value) - 1;
+    buscarPersonaje();
+});
 
-    console.log(buscar);
-    
+function buscarPersonaje() {
+    let ver = document.getElementById("output");
+    let buscar = parseInt(document.getElementById("buscar").value);
 
-fetch(`https://api.disneyapi.dev/character/${buscar}`)
-    .then(res => res.json())
-    .then(Datos => {
-
-        ver.innerHTML = `
-        <td><p id = "nombre">${Datos.data.name}</p>
-        <br><td><p id = "movie">Películas: ${Datos.data.films}</p>
-        <br><td><p id = "fecha">Creado: ${Datos.data.createdAt}</p>
-        <br><td><p id = "game">Videojuegos: ${Datos.data.videoGames}</p>
-        <td><img id="imagen" src= "${Datos.data.imageUrl}"><br>
-        <br> <td><p id = "short"> Short Films: ${Datos.data.shortFilms}
-    `
-
-        
-    })
-}) 
-
-
+    fetch(`https://api.disneyapi.dev/character/` + buscar)
+        .then(res => res.json())
+        .then(Datos => {
+            ver.innerHTML = `
+                <tr><p id="Id">${Datos.data._id}</p></tr>
+                <tr><p id="nombre">${Datos.data.name}</p></tr>
+                <br><tr><p id="movie">Películas: ${Datos.data.films}</p>
+                <br><td><p id="fecha">Creado: ${Datos.data.createdAt}</p>
+                <br><td><p id="game">Videojuegos: ${Datos.data.videoGames}</p>
+                <td><img id="imagen" src="${Datos.data.imageUrl}"><br>
+                <br><td><p id="short"> Short Films: ${Datos.data.shortFilms}
+            `;
+        })
+        .catch(error => {
+            ver.innerHTML = `<p>Error al obtener los datos.</p>`;
+            console.error("Error:", error);
+        });
+}
