9448174a0581af04cb0c2c6ab8244c940a0f55da
wilmer <wilmer7522@gmail.com>
2024-09-12 13:04:54 -0500

    feat :sparkles: writing code

--- Dia13/Script/script.js
+++ Dia13/Script/script.js
@@ -3,129 +3,116 @@ let url1 = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
 let carta1Player1 = document.querySelector(".carta1Player1")
 
 let carta1Computer = document.querySelector(".carta1Computer")
-
+let select = document.getElementById("select")
+let uno = document.getElementById("uno").value
+let once = document.getElementById("once").value
+let pedir = document.getElementById("pedir")
+let sumaPlayer = 0;
+let sumaComputer = 0;
 fetch(url1)
     .then(res => res.json())
     .then(mazo => {
 
-        console.log(mazo.deck_id);
-
         const ulti = mazo.deck_id
-
+        //Player1
 
         fetch(`https://deckofcardsapi.com/api/deck/${ulti}/draw/?count=2`)
             .then(res => res.json())
             .then(cartas => {
-
+                let suma1 = 0;
+                let unoVariable = 0;
+                let onceVariable = 0;
                 cartas.cards.forEach(card => {
                     carta1Player1.innerHTML += `<div class="cartas1Player1"><img src="${card.images.png}"/></div>`
+
+
                     if (card.value === "ACE") {
-                        carta1Player1.innerHTML += 11
-                    }
-                    if (card.value === "KING" || card.value === "QUEEN" || card.value === "JACK") {
-                        carta1Player1.innerHTML += 10
-                    }
-                    if (card.value === "10") {
-                        carta1Player1.innerHTML += 10
-                    }
-                    if (card.value === "9") {
-                        carta1Player1.innerHTML += 9
-                    }
-                    if (card.value === "8") {
-                        carta1Player1.innerHTML += 8
-                    }
-                    if (card.value === "7") {
-                        carta1Player1.innerHTML += 7
-                    }
-                    if (card.value === "6") {
-                        carta1Player1.innerHTML += 6
-                    }
-                    if (card.value === "5") {
-                        carta1Player1.innerHTML += 5
-                    }
-                    if (card.value === "4") {
-                        carta1Player1.innerHTML += 4
-                    }
-                    if (card.value === "3") {
-                        carta1Player1.innerHTML += 3
-                    }
-                    if (card.value === "2") {
-                        carta1Player1.innerHTML += 2
-                    }
-
-                
-               // carta1Player1.innerHTML += card.value
-
-            })
-        console.log(cartas.cards[0].value);
-        console.log(cartas.cards[1].value);
-
-        fetch(`https://deckofcardsapi.com/api/deck/${ulti}/draw/?count=2`)
-            .then(res => res.json())
-            .then(cartas => {
-                let suma = 0;
-                cartas.cards.forEach(card2 => {
-                    carta1Computer.innerHTML += `<div class="cartas1Computer"><img src="${card2.images.png}"/></div>`
-                    if (card2.value === "ACE") {
-                        carta1Computer.innerHTML += 11
-                    }
-                    if (card2.value === "KING" || card2.value === "QUEEN" || card2.value === "JACK") {
-                        carta1Computer.innerHTML += 10
-                    }
-
-                    else {carta1Computer.innerHTML += parseInt(`${card2.value}`)}
-                    /*if (card2.value === "10") {
-                        carta1Computer.innerHTML += 10
-                    }
-                   /* if (card2.value === "9") {
-                        carta1Computer.innerHTML += 9
-                    }
-                    if (card2.value === "8") {
-                        carta1Computer.innerHTML += 8
-                    }
-                    if (card2.value === "7") {
-                        carta1Computer.innerHTML += 7
-                    }
-                    if (card2.value === "6") {
-                        carta1Computer.innerHTML += 6
-                    }
-                    if (card2.value === "5") {
-                        carta1Computer.innerHTML += 5
-                    }
-                    if (card2.value === "4") {
-                        carta1Computer.innerHTML += 4
-                    }
-                    if (card2.value === "3") {
-                        carta1Computer.innerHTML += 3
-                    }
-
-                    if (card2.value === "2") {
-                        carta1Computer.innerHTML += 2
                         
-                    }*/
+                            card.value = 11
+                        }
                     
+                        console.log(parseInt(card.value));
+                        
+                    if (card.value === "KING" || card.value === "QUEEN" || card.value === "JACK") {
+                        card.value = 10
+                    }
+                    else { carta1Player1.innerHTML += parseInt(`${card.value}`) }
+                    if (suma1 === 21) {
+                        alert("Has Ganado")};
 
-                    suma += Number(card2.value)
-                    
-                    console.log(suma);
-                    if (suma > 21) {
-                        alert("Perdiste");}
-                        else if  (suma === 21) {
-                            alert("Ganaste");}
-                    //carta1Computer.innerHTML += card2.value
+
+
+                    sumaPlayer += parseInt(card.value)
+
+
                 })
+                pedir.addEventListener("click", () => {
+                    fetch(`https://deckofcardsapi.com/api/deck/${ulti}/draw/?count=1`)
+                        .then(res => res.json())
+                        .then(pide => {
+
+                            pide.cards.forEach(pide1 => {
+                                carta1Player1.innerHTML += `<div class="cartas1Player1"><img src="${pide1.images.png}"/></div>`
+                                console.log(sumaPlayer);
+                
+                                sumaPlayer += Number(pide1.value)
+                
+                                console.log(sumaPlayer);
+                                if (sumaPlayer > 21) {
+                                    alert("Perdiste");
+                                }
+                                else if (sumaPlayer === 21) {
+                                        alert("Has Ganado")};
+                            })
+
+                        })
+                })
+                
+                //Computer====================================
+
+                fetch(`https://deckofcardsapi.com/api/deck/${ulti}/draw/?count=2`)
+                    .then(res => res.json())
+                    .then(cartas => {
+                        let suma = 0;
+                        cartas.cards.forEach(card2 => {
+                            carta1Computer.innerHTML += `<div class="cartas1Computer"><img src="${card2.images.png}"/></div>`
+                            if (card2.value === "ACE") {
+
+                                card2.value = 11
 
 
-                console.log(parseInt(cartas.cards[0].value));
-                console.log(parseInt(cartas.cards[1].value));
+                            }
+
+
+                            if (card2.value === "KING" || card2.value === "QUEEN" || card2.value === "JACK") {
+                                card2.value = 10
+                            }
+
+                            else { carta1Computer.innerHTML += parseInt(`${card2.value}`) }
+
+
+                            suma += Number(card2.value)
+
+
+                            if (suma > 21) {
+                                alert("Perdio Computadora");
+                            }
+                            else if (suma === 21) {
+                                alert("Gano computadora");
+                            }
+                            //carta1Computer.innerHTML += card2.value
+                        })
+
+
+
+
+
+                    })
+
+
+
 
 
             })
 
-
-
-
-
-    })
-        
     })
