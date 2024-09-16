2655b6f8b89fdf4c3a3c997143672200c7c88eea
wilmer <wilmer7522@gmail.com>
2024-09-16 10:17:25 -0500

    feat :sparkles: writing code

--- Dia13/Script/script.js
+++ Dia13/Script/script.js
@@ -7,6 +7,8 @@ let select = document.getElementById("select")
 let uno = document.getElementById("uno").value
 let once = document.getElementById("once").value
 let pedir = document.getElementById("pedir")
+let mensaje = document.getElementById("mensaje")
+let quedarme = document.getElementById("quedarme")
 let sumaPlayer = 0;
 let sumaComputer = 0;
 fetch(url1)
@@ -20,10 +22,8 @@ fetch(url1)
             .then(res => res.json())
             .then(cartas => {
                 let suma1 = 0;
-                let unoVariable = 0;
-                let onceVariable = 0;
                 cartas.cards.forEach(card => {
-                    carta1Player1.innerHTML += `<div class="cartas1Player1"><img src="${card.images.png}"/></div>`
+                    carta1Player1.innerHTML += `<div class="cartas1Player1" style="display: block;"><img src="${card.images.png}"/></div>`
 
 
                     if (card.value === "ACE") {
@@ -31,20 +31,20 @@ fetch(url1)
                             card.value = 11
                         }
                     
-                        console.log(parseInt(card.value));
                         
-                    if (card.value === "KING" || card.value === "QUEEN" || card.value === "JACK") {
+                        
+                    else if (card.value === "KING" || card.value === "QUEEN" || card.value === "JACK") {
                         card.value = 10
                     }
+                    
+
                     else { carta1Player1.innerHTML += parseInt(`${card.value}`) }
+
                     if (suma1 === 21) {
                         alert("Has Ganado")};
-
-
-
+                        
                     sumaPlayer += parseInt(card.value)
-
-
+                    console.log(parseInt(sumaPlayer));
                 })
                 pedir.addEventListener("click", () => {
                     fetch(`https://deckofcardsapi.com/api/deck/${ulti}/draw/?count=1`)
@@ -52,23 +52,47 @@ fetch(url1)
                         .then(pide => {
 
                             pide.cards.forEach(pide1 => {
-                                carta1Player1.innerHTML += `<div class="cartas1Player1"><img src="${pide1.images.png}"/></div>`
-                                console.log(sumaPlayer);
+                                carta1Player1.innerHTML += `<div class="cartas1Player1" style="display: block;" ><img src="${pide1.images.png}"/></div>`
+                                if (pide1.value === "ACE") {
+                        
+                                    pide1.value = 11
+                                }
+                            
+                                
+                                
+                            else if (pide1.value === "KING" || pide1.value === "QUEEN" || pide1.value === "JACK") {
+                                pide1.value = 10
+                                
+                            }
+                            
+        
+                            else { carta1Player1.innerHTML += parseInt(`${pide1.value}`) }
                 
                                 sumaPlayer += Number(pide1.value)
                 
                                 console.log(sumaPlayer);
                                 if (sumaPlayer > 21) {
-                                    alert("Perdiste");
+                                    mensaje.innerHTML = `<div id="mensaje"><p>Perdiste</p></div>`
+                                    pedir.style.display = "none"
+                                    
                                 }
                                 else if (sumaPlayer === 21) {
-                                        alert("Has Ganado")};
+                                        mensaje.innerHTML = `<div id="mensaje"><p>Gano</p></div>`
+                                        pedir.style.display = "none"
+                                        
+                                    }
                             })
 
                         })
                 })
                 
                 //Computer====================================
+            
+
+                
+
+
+                
 
                 fetch(`https://deckofcardsapi.com/api/deck/${ulti}/draw/?count=2`)
                     .then(res => res.json())
@@ -92,26 +116,67 @@ fetch(url1)
 
 
                             suma += Number(card2.value)
-
-
                             if (suma > 21) {
-                                alert("Perdio Computadora");
+                                carta1Player1.innerHTML = `<div id="mensaje"><p>Perdiste</p></div>`
                             }
                             else if (suma === 21) {
-                                alert("Gano computadora");
+                                carta1Player1.innerHTML = `<div id="mensaje"><p>Gano</p></div>`
                             }
-                            //carta1Computer.innerHTML += card2.value
+                            
+                            
                         })
+                        quedarme.addEventListener("click", ()=>{
 
+                            function aleatorio(inferior, superior) {
+                                var numPosibilidades = superior - inferior;
+                                var aleatorio = Math.random() * (numPosibilidades + 1);
+                                aleatorio = Math.floor(aleatorio);
+                                return inferior + aleatorio;
+                            }
 
+                            aleatorio()
 
+                            fetch(`https://deckofcardsapi.com/api/deck/${ulti}/draw/?count=1`)
+                        .then(res => res.json())
+                        .then(pide => {
 
+                            pide.cards.forEach(pide1 => {
+                                carta1Computer.innerHTML += `<div class="cartas1Computer" style="display: block;" ><img src="${pide1.images.png}"/></div>`
+                                if (pide1.value === "ACE") {
+                        
+                                    pide1.value = 11
+                                }
+                            
+                                
+                                
+                            else if (pide1.value === "KING" || pide1.value === "QUEEN" || pide1.value === "JACK") {
+                                pide1.value = 10
+                                
+                            }
+                            
+        
+                            else { carta1Computer.innerHTML += parseInt(`${pide1.value}`) }
+                
+                                sumaPlayer += Number(pide1.value)
+                
+                                console.log(sumaPlayer);
+                                if (sumaPlayer > 21) {
+                                    mensaje.innerHTML = `<div id="mensaje"><p>Computadora Perdio</p></div>`
+                                    pedir.style.display = "none"
+                                    
+                                }
+                                else if (sumaPlayer === 21) {
+                                        mensaje.innerHTML = `<div id="mensaje"><p>Computadora Gano</p></div>`
+                                        pedir.style.display = "none"
+                                        
+                                    }
+                            })
+
+                        })
 
                     })
 
-
-
-
+                })
 
             })
 
